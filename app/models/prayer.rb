1feec9bf277b7d075fabac7d5b6883d382a14d03
complikatyed <katye.russell@gmail.com>
2015-10-23 13:19:25 -0500

    Change display of prayer requests to include date and time posted.

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -4,4 +4,8 @@ class Prayer < ActiveRecord::Base
     Prayer.where("created_at >= ?", 1.week.ago.utc).order("created_at DESC")
   end
 
+  def title=(new_title)
+    write_attribute(:title, new_title.upcase)
+  end
+
 end
