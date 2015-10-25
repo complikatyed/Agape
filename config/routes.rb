988cf8776e992a9debc32236e8518548ebd02d9b
complikatyed <katye.russell@gmail.com>
2015-10-24 21:44:38 -0500

    Add _header and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-root 'welcome#index'
-
+root             'welcome#index'
+get 'help'    => 'welcome#help'
 resources :prayers
 
 end
