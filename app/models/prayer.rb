5c9e7eacdeeabee452b55619d64798c8f3b79f23
complikatyed <katye.russell@gmail.com>
2015-10-24 21:49:01 -0500

    Remove timestamp from prayer requests view.

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -4,8 +4,5 @@ class Prayer < ActiveRecord::Base
     Prayer.where("created_at >= ?", 1.week.ago.utc).order("created_at DESC")
   end
 
-  def title=(new_title)
-    write_attribute(:title, new_title.upcase)
-  end
 
 end
