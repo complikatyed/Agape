e1e1a2f5029e2497d0375a2db7ef5f68ba1b4717
complikatyed <katye.russell@gmail.com>
2015-10-26 21:06:52 -0500

    Add social page with route and change welcome index to include new path.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
 root             'welcome#index'
 get 'help'    => 'welcome#help'
+get 'social'  => 'welcome#social'
 resources :emails
 resources :prayers
+resources :lessons
 
 
 end
