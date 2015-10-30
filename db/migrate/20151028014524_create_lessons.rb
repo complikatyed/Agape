c0ecd781622e947a216c6e6cee9c7655629f2a7d
complikatyed <katye.russell@gmail.com>
2015-10-29 23:24:59 -0500

    Add migration for lesson model and seeds for class emails.

--- db/migrate/20151028014524_create_lessons.rb
+++ db/migrate/20151028014524_create_lessons.rb
@@ -1,6 +1,9 @@
 class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
+      t.string :date
+      t.string :scripture
+      t.text :description
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025201346) do
+ActiveRecord::Schema.define(version: 20151028014524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20151025201346) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string   "date"
+    t.string   "scripture"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "prayers", force: :cascade do |t|
