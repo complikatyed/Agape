93b1ad298cedbea873b507b35d43fec38d1c44a7
complikatyed <katye.russell@gmail.com>
2015-10-30 21:29:34 -0500

    Finish pretty routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 root             'welcome#index'
 get 'help'    => 'welcome#help'
 get 'social'  => 'welcome#social'
-get 'signup', => 'users#new'
-get 'login',  => 'sessions#new'
-get 'logout', => 'sessions#destroy'
+get 'signup'  => 'users#new'
+get 'login'   => 'sessions#new'
+get 'logout'  => 'sessions#destroy'
 
 resources :emails
 resources :lessons
