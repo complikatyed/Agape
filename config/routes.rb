d3199409ae74693e9ae12753cbd5f080ffcf51da
complikatyed <katye.russell@gmail.com>
2015-10-30 20:08:19 -0500

    Undo users signup steps. Starting over to follow revised Railscast approach.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
 root             'welcome#index'
 get 'help'    => 'welcome#help'
 get 'social'  => 'welcome#social'
-resources :emails
-resources :prayers
-resources :lessons
 
+resources :emails
+resources :lessons
+resources :prayers
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030230741) do
+ActiveRecord::Schema.define(version: 20151028014524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,14 +37,6 @@ ActiveRecord::Schema.define(version: 20151030230741) do
     t.string   "postedby"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
 end
