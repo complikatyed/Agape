6b907355d3c0f4558d1f4fec652520db70cb26fd
complikatyed <katye.russell@gmail.com>
2015-10-30 21:25:08 -0500

    Make the paths pretty.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
 root             'welcome#index'
 get 'help'    => 'welcome#help'
 get 'social'  => 'welcome#social'
+get 'signup', => 'users#new'
+get 'login',  => 'sessions#new'
+get 'logout', => 'sessions#destroy'
 
 resources :emails
 resources :lessons
 resources :prayers
+resources :sessions
 resources :users
 
 end
