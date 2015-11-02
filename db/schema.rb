2a313a704bd8c783939abbfe7d2b2a4b7c9f150a
complikatyed <katye.russell@gmail.com>
2015-11-01 21:10:38 -0600

    Add migration for auth_token and run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031011005) do
+ActiveRecord::Schema.define(version: 20151102030750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20151031011005) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "auth_token"
   end
 
 end
